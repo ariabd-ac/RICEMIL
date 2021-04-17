edbc81f37b899c417d9fde366e37c23b3041fe89
ariabd-ac <ariabd.com@gmail.com>
2021-04-17 21:32:41 +0700

    r

--- reseller/barang/index.php
+++ reseller/barang/index.php
@@ -1,31 +1,33 @@
 <div class="row">
-<?php
-    $query="SELECT * FROM tb_barang";
-    $res=mysqli_query($conn,$query);
+    <?php
+    $query = "SELECT * FROM tb_barang";
+    $res = mysqli_query($conn, $query);
 
-    if(!$res){
-        die('err'.mysqli_error($conn));
+    if (!$res) {
+        die('err' . mysqli_error($conn));
     }
 
-    while($r=mysqli_fetch_assoc($res)){  
-?>
-    <div class="col-md-3">
-        <div class="card">
-            <div class="card-body">
-                <div class='d-flex align-center justify-content-center'>
-                    <img src="http://localhost/ricemil/assets/images/produk/<?php echo $r['gambar']?>" alt="" width="350px">
+    while ($r = mysqli_fetch_assoc($res)) {
+    ?>
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class='d-flex align-center justify-content-center'>
+                        <a class='bg-image hover-overlay' href="/ricemil/reseller/index.php?page=barang&modul=detail&id=<?php echo $r['Id_barang']; ?>" style="text-decoration: none;">
+                            <img src="http://localhost/ricemil/assets/images/produk/<?php echo $r['gambar'] ?>" alt="" width="350px">
+                        </a>
+                    </div>
+                    <div class="desc">
+                        <h4><?php echo $r['Nama_barang'] ?></h4>
+                        <p>Stock <?php echo $r['stock'] ?></p>
+                        <p>Rp. <?php echo $r['harga'] ?></p>
+
+                    </div>
                 </div>
-                <div class="desc">
-                    <h4><?php echo $r['Nama_barang']?></h4>
-                    <p>Stock <?php echo $r['stock']?></p>
-                    <p>Rp. <?php echo $r['harga']?></p>
-                  
+                <div class="card-footer">
+                    <!-- <a class='btn btn-info' href="/ricemil/reseller/index.php?page=barang&modul=detail&id=<?php echo $r['Id_barang']; ?>">Detail</a> -->
                 </div>
-            </div>
-            <div class="card-footer">
-                <a class='btn btn-info' href="/ricemil/reseller/index.php?page=barang&modul=detail&id=<?php echo $r['Id_barang'];?>">Detail</a>
             </div>
         </div>
-    </div>
-<?php } ?>
+    <?php } ?>
 </div>
