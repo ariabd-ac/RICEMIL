95fbc0bc021ac4caf7af470c51fdd274ff454964
rizalazky <rizalazky26@gmail.com>
2021-07-30 01:31:01 +0700

    forget password

--- forget_password_form.php
+++ forget_password_form.php
@@ -2,38 +2,24 @@
 
 include 'config/koneksi.php';
 session_start();
-
+$email=$_GET['email'];
 if(isset($_POST['submit'])){
-    $to=$_POST['email'];
-    // die($to);
-    $six_digit_random_number = random_int(100000, 999999);
-    $subject = "My subject";
-    $txt = "Kode Ganti Email = ".$six_digit_random_number;
-    $headers = "From: ameliaricemil@gmail.com" . "\r\n" .
-    "CC: rizalazky26@gmail.com";
+    $newPassword=md5($_POST['password']);
+    $kode=$_POST['kode'];
 
-    $queryCheckIfEmailExist="SELECT email FROM users WHERE email='$to'";
-    $exec=mysqli_query($conn,$queryCheckIfEmailExist);
-    // die(var_dump($exec));
-    if(mysqli_num_rows($exec)>0){
-        $send=mail($to,$subject,$txt,$headers);
-            // die($send);
-        if(!$send){
-            die(print_r(error_get_last()));
-        }else{
-            $queryUpdate="UPDATE users SET kode_change_password='$six_digit_random_number' WHERE email='$to'";
-            $exUp=mysqli_query($conn,$queryUpdate);
-            if(!$exUp){
-                die(mysqli_error($conn));
-            }
-            header('Location: forget_password_form.php?email='.$to);
-        }
+    $check="SELECT * FROM users WHERE kode_change_password='$kode' AND email='$email'";
+
+    $exec=mysqli_query($conn,$check);
+
+    if(mysqli_num_rows($exec) > 0){
+        $qUpdatePw="UPDATE users SET password='$newPassword',kode_change_password=NULL WHERE email='$email'";
+
+        $execUdatePw=mysqli_query($conn,$qUpdatePw);
+
+        die($execUdatePw);
     }else{
-        echo "<script>alert('Email Belum Terdaftar !!')</script>";
+        die($email." && ".$kode);
     }
-
-
-
 }
 
 ?>
@@ -45,7 +31,7 @@ if(isset($_POST['submit'])){
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Login</title>
+    <title>Ganti Password</title>
 
     <link rel="stylesheet" href="./assets/css/login.css" />
     <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -60,8 +46,10 @@ if(isset($_POST['submit'])){
                 <div class="error-text"></div>
                 <div class="content-register animate__animated animate__fadeInLeft">
                     <div class="register-row-top">
-                        <h6>Email</h6>
-                        <input class="username" name="email" type="email" placeholder="Email" />
+                        <h6>New Password</h6>
+                        <input class="username" name="password" type="password" placeholder="new password" />
+                        <h6>Kode</h6>
+                        <input class="username" name="kode" type="text" placeholder="kode yang dikirim ke email" />
                     </div>
                 </div>
                 
